5c05db898d0df2a70e1017f0fc487efd80d36536
andika wahyudi <andikawpb200@gmail.com>
2024-03-16 00:03:39 +0700

    style: polishing design to be more tidy

--- src/App.jsx
+++ src/App.jsx
@@ -34,18 +34,18 @@ function App() {
           <div className="flex justify-between items-center border-y border-gray-400 dark:border-neutral-400 py-5 px-5 lg:px-0">
             <div className="hidden lg:block space-x-4">
               <a href="#andika-info" className="underline">
-                INFO
+                About Mex
               </a>
               <span>/</span>
               <a href="#andika-projects" className="underline">
-                PROJECTS[6]
+                Projects[6]
               </a>
               <span>/</span>
               <a
                 href="/resume/Andika-Wahyudi-CV-2024.pdf"
                 className="underline"
               >
-                RESUME
+                Resume
               </a>
             </div>
 
@@ -56,7 +56,7 @@ function App() {
 
               <span>/</span>
               <a href="#andika-contact" className="underline">
-                CONTACT
+                Contact
               </a>
 
               <span>/</span>
@@ -65,7 +65,7 @@ function App() {
                 className="border border-gray-400 dark:border-neutral-400 px-4 py-2"
                 onClick={toggleTheme}
               >
-                {theme}
+                {`#include<${theme}-THEME.h>`}
               </button>
             </div>
 
@@ -73,7 +73,7 @@ function App() {
               className="block lg:hidden border border-gray-400 dark:border-neutral-400 px-4 py-2"
               onClick={() => setShowMenu((prev) => !prev)}
             >
-              MENU
+              Menu
             </button>
 
             <button
@@ -86,17 +86,17 @@ function App() {
 
           {showMenu && (
             <div className="grid grid-cols-2 divide-x divide-gray-400 dark:divide-neutral-400 lg:hidden border-b border-gray-400 dark:border-neutral-400">
-              <div className="uppercase p-5 space-y-4">
+              <div className="p-5 space-y-4">
                 <p className="font-bold">Navigation</p>
 
                 <div>
                   <a href="#andika-info" className="underline">
-                    INFO
+                    About Me
                   </a>
                 </div>
                 <div>
                   <a href="#andika-projects" className="underline">
-                    PROJECTS[6]
+                    Projects[6]
                   </a>
                 </div>
                 <div>
@@ -104,22 +104,22 @@ function App() {
                     href="/resume/Andika-Wahyudi-CV-2024.pdf"
                     className="underline"
                   >
-                    RESUME
+                    Resume
                   </a>
                 </div>
               </div>
 
-              <div className="uppercase p-5 space-y-4">
-                <p className="font-bold">Navigation</p>
+              <div className="p-5 space-y-4">
+                <p className="font-bold">More</p>
 
                 <div>
                   <a href="mailto:andikawpb200@gmail.com" className="underline">
-                    EMAIL ME
+                    Email Me
                   </a>
                 </div>
                 <div>
                   <a href="#andika-contact" className="underline">
-                    CONTACT
+                    Contact
                   </a>
                 </div>
               </div>
@@ -131,39 +131,67 @@ function App() {
           <div>
             <img src="/me.jpg" alt="" />
           </div>
-          <div className="space-y-5 uppercase">
-            <p className="leading-6 text-justify">
-              &nbsp;&nbsp;&nbsp;&nbsp; I am a seasoned software engineer with 1
-              years of experience in the field. Passionate about software
-              engineering, I thrive on challenges encountered during the
-              development process. To me, software engineering is not just a
-              profession, but a vibrant passion that ignites creativity and
-              innovation. Whether tackling complex algorithms or architecting
-              elegant solutions, I approach each task with enthusiasm and
-              dedication.
+          <div className="space-y-5">
+            <p className="leading-6 text-start">
+              I am a seasoned software engineer with 1 years of experience in
+              the field. Passionate about software engineering, I thrive on
+              challenges encountered during the development process. To me,
+              software engineering is not just a profession, but a vibrant
+              passion that ignites creativity and innovation. Whether tackling
+              complex algorithms or architecting elegant solutions, I approach
+              each task with enthusiasm and dedication.
             </p>
 
-            <p>Clients:</p>
+            <div className="space-y-1 font-[consolas] bg-white dark:bg-black p-4 border border-gray-300 dark:border-slate-700 rounded-md">
+              <div>AndikaClients.java</div>
+              <hr />
+              <br />
+              <p>{"LinkedList<String> myClients = new LinkedList<>()"}</p>
+              <p>
+                myRecentClientsList.add(
+                <a href="https://sistemparkir.com" className="underline">
+                  "Media Kreasi Utama"
+                </a>
+                )
+              </p>
+              <p>
+                myRecentClientsList.add(
+                <a href="https://sistemparkir.com" className="underline">
+                  "Sistem Kreasi Indonesia"
+                </a>
+                )
+              </p>
+              <br />
+              System.out.print(myClients);
+            </div>
 
-            <p className="space-x-3">
-              <a href="https://sistemparkir.com" className="underline">
-                Media Kreasi Utama
-              </a>
-              <span>/</span>
-              <a href="https://sistemparkir.com" className="underline">
-                Sistem Kreasi Indonesia
-              </a>{" "}
-            </p>
+            <div className="space-y-1 font-[consolas] bg-white dark:bg-black p-4 border border-gray-300 dark:border-slate-700 rounded-md">
+              <div>Console Output (AndikaClients.class):</div>
+              <hr />
+              <br />
+
+              <p>
+                {"["}
+                <a href="https://sistemparkir.com" className="underline">
+                  "Media Kreasi Utama"
+                </a>
+                <span>,</span>
+                <a href="https://sistemparkir.com" className="underline">
+                  "Sistem Kreasi Indonesia"
+                </a>
+                {"]"}
+              </p>
+            </div>
           </div>
-          <div className="space-y-5 uppercase">
-            <p className="leading-6 text-justify">
-              &nbsp;&nbsp;&nbsp;&nbsp; Consistency, reliability, and client
-              satisfaction define my approach to software engineering. The
-              software solutions I develop are not merely good—they exceed
-              expectations, offering robust functionality, seamless performance,
-              and intuitive user experiences. My track record speaks for itself,
-              as I continually strive to uphold the highest standards of
-              excellence in every project I undertake.
+          <div className="space-y-5">
+            <p className="leading-6 text-start">
+              Consistency, reliability, and client satisfaction define my
+              approach to software engineering. The software solutions I develop
+              are not merely good—they exceed expectations, offering robust
+              functionality, seamless performance, and intuitive user
+              experiences. My track record speaks for itself, as I continually
+              strive to uphold the highest standards of excellence in every
+              project I undertake.
             </p>
           </div>
         </div>
